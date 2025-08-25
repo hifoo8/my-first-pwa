50a587ce963fe6df525a3aa681753f56ba33695d
hifoo8 <71152155+hifoo8@users.noreply.github.com>
2025-08-25 21:14:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker: Installed");
+const CACHE_NAME = 'shop-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './product1.jpg',
+  './product2.jpg'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  console.log("Service Worker: Fetching", e.request.url);
+// Fetch cached files if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
